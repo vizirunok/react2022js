adb82884abb28636fbbb694c80ffc7de17f8ebb8
vizirunok <andry.maistrenko@gmail.com>
2022-09-25 10:43:08 +0300

    lsss

--- src/App.js
+++ src/App.js
@@ -1,11 +1,15 @@
-import './App.css';
+import {useEffect} from "@types/react";
+import {getUsers} from "./services/userService";
+
 
 function App() {
+  useEffect(() => {
+    getUsers().then(value => console.log(value));
+  },[])
   return (
-    <div className="App">
+    <div>
 
     </div>
   );
 }
-
 export default App;
--- src/services/userService.js
+++ src/services/userService.js
@@ -1,3 +1,14 @@
+import axios from "axios";
+
+let axiosInstance = axios.create({
+    baseURL: 'https://jsonplaceholder.typicode.com'
+});
+
+const getUsers = () => {
+    return axiosInstance.get('/users')
+};
+
+export {getUsers};
 
 
 
