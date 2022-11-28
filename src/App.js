a6ba550c072c8d74d4650fa6ec78cd2a24851c29
vizirunok <andry.maistrenko@gmail.com>
2022-11-28 12:23:06 +0200

    completed

--- src/App.js
+++ src/App.js
@@ -2,7 +2,6 @@ import './App.css';
 import {Items} from "./less1/hw/simpsons/items";
 import RandM from "./less1/hw/r&m/actors";
 
-
 function App() {
     return (
         <div>
@@ -14,9 +13,3 @@ function App() {
 
 export default App;
 
-// 1. Описати всю сім'ю сімпсонів (5 персонажів)
-//
-// 2. Створити компонент який описує персонажа (властивості id,name,status,species,gender,image) з цього апі
-// https://rickandmortyapi.com/
-//     https://rickandmortyapi.com/api/character
-//         Створити 6 персонажів
