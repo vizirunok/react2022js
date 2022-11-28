a73b1d505a464eaa7645cf09789596e62df5ceee
vizirunok <andry.maistrenko@gmail.com>
2022-11-28 10:30:18 +0200

    write

--- src/App.js
+++ src/App.js
@@ -1,8 +1,17 @@
+import {useEffect, useState} from "react";
 
 function App() {
+    let [users, setUsers] = useState([]);
+    useEffect(() =>
+        fetch('https://jsonplaceholder.typicode.com/users')
+            .then(value => value.json())
+            .then(value => setUsers(value))
+        ,[])
     return(
     <div>
-
+        {
+            console.log(users)
+        }
     </div>
 )}
 
