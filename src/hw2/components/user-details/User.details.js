ee8c48347014d8b00ff080f964f5715d0d9390ac
vizirunok <andry.maistrenko@gmail.com>
2022-12-02 14:49:12 +0200

    uuj

---
+++ src/hw2/components/user-details/User.details.js
@@ -0,0 +1,9 @@
+function UserDetails({dop}) {
+    return(
+        <div>
+            <h1>{dop.id} {dop.name}</h1>
+        </div>
+    )
+}
+
+export {UserDetails};
--- src/hw2/components/users/Users.js
+++ src/hw2/components/users/Users.js
@@ -2,32 +2,33 @@ import {useEffect, useState} from "react";
 
 import {User} from "../user/User";
 import {userService} from "../../services/user.service";
+import {UserDetails} from "../user-details/User.details";
 
 
 export function Users() {
 
     let [users, setUsers] = useState([]);
 
-    // let [dop, setDop] = useState(null);
-    //
-    // const details = (info) => {
-    //     return setDop(info)
-    // }
+    let [dop, setDop] = useState(null);
+
+    const details = (info) => {
+        return setDop(info)
+    }
 
 
     useEffect(() => {
-        userService.getAll().then(value => console.log(value.data));
+        userService.getAll().then(value => setUsers(value.data));
     }, []);
 
     return(
         <div>
             <hr/>
-            {/*{*/}
-            {/*    users.map(value => (<User key={value.id} value={value} details={details}/>))*/}
-            {/*}*/}
-            {/*{*/}
-            {/*    dop && (<div>{dop.name}</div>)*/}
-            {/*}*/}
+            {
+                users.map(value => (<User key={value.id} value={value} details={details}/>))
+            }
+            {
+                dop && (<UserDetails dop={dop} key={dop.id}/>)
+            }
         </div>
     )
 }
