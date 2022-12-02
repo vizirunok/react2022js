805642d8dca638ad339b2d48ff511d7ccd20b19c
vizirunok <andry.maistrenko@gmail.com>
2022-12-02 13:51:15 +0200

    ffff

--- src/hw2/components/users/Users.js
+++ src/hw2/components/users/Users.js
@@ -3,29 +3,30 @@ import {useEffect, useState} from "react";
 import {User} from "../user/User";
 import {userService} from "../../services/user.service";
 
+
 export function Users() {
 
     let [users, setUsers] = useState([]);
 
-    let [dop, setDop] = useState(null);
-
-    const details = (info) => {
-        return setDop(info)
-    }
+    // let [dop, setDop] = useState(null);
+    //
+    // const details = (info) => {
+    //     return setDop(info)
+    // }
 
     useEffect(() => {
-        userService.getAll().then(({data}) => setUsers(data))
+        userService.then(value => console.log(value));
     }, []);
 
     return(
         <div>
             <hr/>
-            {
-                users.map(value => (<User key={value.id} value={value} details={details}/>))
-            }
-            {
-                dop && (<div>{dop.name}</div>)
-            }
+            {/*{*/}
+            {/*    users.map(value => (<User key={value.id} value={value} details={details}/>))*/}
+            {/*}*/}
+            {/*{*/}
+            {/*    dop && (<div>{dop.name}</div>)*/}
+            {/*}*/}
         </div>
     )
 }
