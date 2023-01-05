25acc74f55c7d17c8bfeccff7fcb95d7ce8588c4
vizirunok <andry.maistrenko@gmail.com>
2023-01-05 16:14:46 +0200

    sss

--- src/components/UserForm/UserForm.js
+++ src/components/UserForm/UserForm.js
@@ -2,24 +2,27 @@ import {useForm} from "react-hook-form";
 import {joiResolver} from "@hookform/resolvers/joi";
 
 import {UserValidator} from "../../validators";
+import {UserServices} from "../../services";
 
 
-const UserForm = () => {
+const UserForm = ({setUsers}) => {
 
     const {register, handleSubmit, reset, formState: {errors, isValid}} = useForm({
         resolver: joiResolver(UserValidator),
         mode: 'all'});
 
 
-    function onSubmit(user) {
-        console.log(user);
-    }
+    const onSubmit = async (car) => {
+        const {data} = await UserServices.create(car);
+        setUsers(users => [...users, data]);
+
+    };
 
     return (
         <form onSubmit={handleSubmit(onSubmit)}>
             <input type="text" placeholder={'name'} {...register('name')}/>
             {errors.name && <span>{errors.name.message}</span>}
-            <input type="text" placeholder={'phone'} {...register('phone', {maxLength: 10})}/>
+            <input type="text" placeholder={'phone'} {...register('phone')}/>
             {errors.phone && <span>{errors.phone.message}</span>}
             <button disabled={!isValid}>Save</button>
         </form>
