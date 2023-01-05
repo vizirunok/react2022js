c5732d4954e4164aa6d0e4cfbf894b1980247f0f
vizirunok <andry.maistrenko@gmail.com>
2023-01-05 12:20:22 +0200

    sss

--- src/validators/user.validator.js
+++ src/validators/user.validator.js
@@ -1,10 +1,10 @@
 import Joi from "joi";
 
 const UserValidator = Joi.object({
-    name: Joi.string().regex(/^[a-zA-Zа-яА-ЯіІїЇєЄґҐ]{1,10}$/).required().messages({
-        'sting.pattern.base': 'Тільки літери, не більше 10 символів'
-    }),
+    name: Joi.string().regex(/^[a-zA-ZА-Яа-яіІїЇєЄґҐ]{1,10}$/).required().messages({
+        'string.pattern.base': 'Тільки літери, не менше 1 та не більше 10 символів'}),
     phone: Joi.string().required(),
 });
 
 export {UserValidator};
+
