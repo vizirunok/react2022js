b2bce9acab8b23460104fd515acbb4aacf297c5a
vizirunok <andry.maistrenko@gmail.com>
2023-01-24 17:05:40 +0200

    asdsdsd

--- src/App.js
+++ src/App.js
@@ -1,25 +1,24 @@
-import logo from './logo.svg';
 import './App.css';
+import {useReducer} from "react";
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+
+
+    const reducer = (state, actions) => {
+
+    };
+
+  const init = () => {
+    return
+  };
+
+  const useReducer1 = useReducer(reducer, 0, init);
+
+    return (
+        <div>
+
+        </div>
+    );
 }
 
 export default App;
