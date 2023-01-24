55dacd8c518b8067eb36e6eb2cdefed346fa68af
vizirunok <andry.maistrenko@gmail.com>
2023-01-24 17:46:03 +0200

    asdsdsd

--- src/App.js
+++ src/App.js
@@ -1,21 +1,40 @@
 import './App.css';
 import {useReducer} from "react";
 
+
+const init = (initState) => {
+    return {count1: initState}
+};
+
+
+const reducer = (state, actions) => {
+    switch (actions.type) {
+        case 'INC':
+            return {count1: state.count1 + 1};
+        case 'DEC':
+            return {count1: state.count1 - 1};
+        case 'RESET':
+            return {count1: 0};
+        case 'SET':
+            return {count1: actions.payload};
+
+    }
+
+};
+
+
 function App() {
 
-
-    const reducer = (state, actions) => {
-
-    };
-
-  const init = () => {
-    return
-  };
-
-  const useReducer1 = useReducer(reducer, 0, init);
+    const [state, dispatch]  = useReducer(reducer, 0, init);
 
     return (
         <div>
+            <h3>Count1: {state.count1}</h3>
+            <button onClick={() => dispatch({type: 'INC'})}>INC</button>
+            <button onClick={() => dispatch({type: 'DEC'})}>DEC</button>
+            <button onClick={() => dispatch({type: 'RESET'})}>RES</button>
+            <button onClick={() => dispatch({type: 'SET', payload: 150})}>SET</button>
+
 
         </div>
     );
